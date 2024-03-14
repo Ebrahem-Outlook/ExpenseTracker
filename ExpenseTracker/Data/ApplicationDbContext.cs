6e360cd2cf1dc4575e5920cb53f836ee6bf95b4e
EbrahemElbetar1 <132098790+EbrahemElbetar1@users.noreply.github.com>
2024-03-14 02:21:07 +0200

    Add project files.

---
+++ ExpenseTracker/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ExpenseTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+    }
+}
